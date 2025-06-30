c072409d0bf8acc3ea84f150c67641b2aedf615d
paulina-pacheco-o <paulimichi05@gmail.com>
2025-06-30 14:53:36 +0200

    first exercise - 01 numbers

--- 01-numbers/script.js
+++ 01-numbers/script.js
@@ -6,7 +6,7 @@ const numThree = 32; // NON MODIFICARE QUESTA RIGA
 // gli operatori numerici di Javascript
 
 // 1. Imposta questa variabile come somma di numOne e numTwo
-const numOnePlusNumTwo = NaN;
+const numOnePlusNumTwo = 8 + 16;
 console.log(numOnePlusNumTwo); // Output: 24
 
 // 2. Imposta questa variabile come prodotto di numThree e numTwo
